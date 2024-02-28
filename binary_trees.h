9439c1c980c83ec9d71b71c5b214f448e92a80c1
NafisaKaruri <nafeesahassan016@gmail.com>
2024-02-28 14:47:17 -0800

    task 26

--- binary_trees.h
+++ binary_trees.h
@@ -20,13 +20,16 @@ struct binary_tree_s
 	struct binary_tree_s *right;
 };
 
+/* types */
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
+/* print funtion */
 void binary_tree_print(const binary_tree_t *);
 
+/* simple binary tree functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -53,6 +56,9 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
+
+/* binary search tree functions */
 bst_t *bst_insert(bst_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
