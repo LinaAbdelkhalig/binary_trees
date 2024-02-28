2577f037a1959544b0cfb58829b713123a230c43
Lina Abdelkhalig <lelsammani0@gmail.com>
2024-02-28 11:43:39 +0200

    fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,11 +12,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (tree->left)
-		left_h = 1 + binary_tree_height(tree->left);
-	if (tree->right)
-		right_h = 1 + binary_tree_height(tree->right);
-	return ((left_h > right_h) ? left_h : right_h);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
+	return ((left_h > right_h) ? left_h + 1 : right_h + 1);
 }
 
 /**
